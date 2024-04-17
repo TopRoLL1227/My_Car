1634aded0b9e6c33a8a2f36bf7e2c845efd61579
Volodymyr <terces@ukr.net>
2024-04-17 13:40:14 +0300

    Change models.py

--- Cars/models.py
+++ Cars/models.py
@@ -1,10 +1,56 @@
 from django.db import models
+import uuid
 
 
-# Create your models here.
+class Brand(models.Model):
+    Audi = 'Audi'
+    Bmw = 'Bmw'
+    Mercedes_Benz = 'Mercedes-Benz'
+    Volkswagen = 'Volkswagen'
+    Skoda = 'Skoda'
+    Hyundai = 'Hyundai'
+    Chevrolet = 'Chevrolet'
+    Ford = 'Ford'
+    Honda = 'Honda'
+    Infinity = 'Infinity'
+    Lexus = 'Lexus'
+    Nissan = 'Nissan'
+    Mazda = 'Mazda'
+    Mitsubishi = 'Mitsubishi'
+    Subaru = 'Subaru'
+    Peugeot = 'Peugeot'
+    Toyota = 'Toyota'
+
+    CAR_CHOICES = [
+        (Audi, 'Audi'),
+        (Bmw, 'Bmw'),
+        (Mercedes_Benz, 'Mercedes-Benz'),
+        (Volkswagen, 'Volkswagen'),
+        (Skoda, 'Skoda'),
+        (Hyundai, 'Hyundai'),
+        (Chevrolet, 'Chevrolet'),
+        (Ford, 'Ford'),
+        (Honda, 'Honda'),
+        (Infinity, 'Infinity'),
+        (Lexus, 'Lexus'),
+        (Nissan, 'Nissan'),
+        (Mazda, 'Mazda'),
+        (Mitsubishi, 'Mitsubishi'),
+        (Subaru, 'Subaru'),
+        (Peugeot, 'Peugeot'),
+        (Toyota, 'Toyota'),
+    ]
+
+    brand_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'Бренд - {self.brand_name}'
+
+
 class Cars(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4(), editable=False)
     model = models.CharField(max_length=100)
-    brand = models.CharField(max_lenght=100)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, blank=False, null=True, verbose_name='brand')
     generation = models.CharField(max_length=100)
     engine = models.FloatField(null=True, blank=True)
     year = models.IntegerField(null=True, blank=True)
