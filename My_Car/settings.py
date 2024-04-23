d4d089b2cb20425f33a1fe770d2b22b1aea08a7b
Volodymyr <terces@ukr.net>
2024-04-23 17:15:02 +0300

    resolve converstation

--- My_Car/settings.py
+++ My_Car/settings.py
@@ -77,13 +77,9 @@ WSGI_APPLICATION = "My_Car.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'my_cars',
-        'USER': 'tester',
-        'PASSWORD': '898989',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
