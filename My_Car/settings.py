e2f3c1f4dd8f530f7c0cecb3c826122a2e165e5d
Volodymyr <terces@ukr.net>
2024-04-23 09:43:44 +0300

    reload

--- My_Car/settings.py
+++ My_Car/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "cars.app.CarsConfig"
+    "cars",
+    "users"
 ]
 
 MIDDLEWARE = [
