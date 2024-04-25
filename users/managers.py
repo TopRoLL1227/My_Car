dc5b805bdf10d8675e1aeb0f332bd692c937da78
Volodymyr <terces@ukr.net>
2024-04-25 14:52:14 +0300

    fix-superuser

--- users/managers.py
+++ users/managers.py
@@ -1,22 +1,20 @@
-from django.db import models
-from django.contrib.auth.models import BaseUserManager
-
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 class UserModelManager(BaseUserManager):
 
-    def create_user(self, email, name, password=None):
+    def create_user(self, email, username, password=None):
         if not email:
             raise ValueError('User must have an email address.')
 
         email = self.normalize_email(email)
-        user = self.model(email=email)
+        user = self.model(email=email, username=username)
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, name, password):
-        user = self.create_user(email, name, password)
+    def create_superuser(self, email, username, password):
+        user = self.create_user(email, username, password)
 
         user.is_superuser = True
         user.is_staff = True
